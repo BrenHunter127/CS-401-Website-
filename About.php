2368cc64db2cd8e5c1518775f861842843e07525
Brendan Hunter <brendanhunter@u.boisestate.edu>
2023-03-22 22:01:34 -0600

    test2

---
+++ About.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+	<title>CS 401 Project</title>
+	<link rel="stylesheet" type="text/css" href="about.css">
+
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+</head>
+
+<body class="about">
+
+	<?php include 'navigation.php'; ?>
+	<div class="wrapper">
+		<div id="body-paragraphs">
+			<p>Howmanyrubles.com is an open source tool kit for Escape from Tarkov</p>
+			<p>This website is designed and maintained to help the community. It can help you with flea market trades, quests, and improving your Tarkov experience.</p>
+		</div>
+
+		<div id="boxes">
+			<div class="box">
+				<h1>Market</h1>
+				<h2>Monitor prices and profit</h2>
+			</div>
+			<div class="box">
+				<h1>Tools</h1>
+				<h2>Calculate market profits</h2>
+			</div>
+			<div class="box">
+				<h1>Maps</h1>
+				<h2>Browse map layouts from above</h2>
+			</div>
+		</div>
+		
+		<div id="footer-line">	
+		</div>
+
+		<div class="footer-container">
+			<div id="footer-line"></div>
+			<div id="footer-trademark"><p>Game content and materials are trademarks and copyrights of HowManyRubels.com All rights reserved.</p></div>
+			<div id="footer-copyright"><p>© 2023 HowManyRubels.com</p></div>
+		</div>
+	</div>
+</body>
+</html>
