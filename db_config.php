c5cf8a9e4e8e78cd518aa82d17acad5c26d0b6f6
Brendan Hunter <brendanhunter@u.boisestate.edu>
2023-04-18 19:43:43 -0600

    database

--- db_config.php
+++ db_config.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-define('DB_HOST', 'ec2-3-208-74-199.compute-1.amazonaws.com');
+define('DB_HOST', 'ec2-34-202-127-5.compute-1.amazonaws.com');
 define('DB_PORT', '5432'); 
-define('DB_NAME', 'daqf1pbq5h99o3');
-define('DB_USER', 'zjaugyusmxjjyo');
-define('DB_PASS', 'f415d3dcf75e8b62b5ea3fb5c6bd103469c7c190783db4a869af69bd7c8d77a8');
+define('DB_NAME', 'd7em4g1rn38mv1');
+define('DB_USER', 'lbkoljrefxctsf');
+define('DB_PASS', '67dc174afda6a9c25830163b827b760b089aefabf1815469b97305223bfaed42');
 define('DB_SSLMODE', 'require'); 
 ?>
